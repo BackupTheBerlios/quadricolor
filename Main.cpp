221c5db8f3c38d3c7461d1b5d80dd75599147da3
madmatt <madmatt>
2003-05-07 19:20:02 +0000

    main avec les templates qui foutent la merde

--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <iostream>
 #include <Magick++.h>
+#include <string>
 #include "LoadingSystem/Loader.hpp"
-#include "CacheSystem/Cache.hpp"
 #include "CacheSystem/CacheFifo.hpp"
 
 using namespace CacheSystem;
 using namespace std;
 using namespace loader;
+using namespace Magick;
 
 int main(int argc, char ** argv)
 {
-  Loader<string> L;
+  Loader<string> l;
   if(argc<2)
     {
       fprintf(stderr, "Syntaxe: quadricolor <image file name>\n");
       exit(EXIT_FAILURE);
     }
-  string k = string(argv[1]);
-  cout << k << endl;
+  string file_name = string(argv[1]);
+  cout << file_name << endl;
+  CacheFifo<Image, string, Loader<string> > cache = CacheFifo<Image, string, Loader<string> >(l);
   try{
-    Image pic = L.getObject(k);
-    cout << "The size of the image " << k << " is " << L.getSize(k) << endl;
+    //Image pic = l.getObject(file_name);
+    Image pic = cache.getImageObject(file_name);
+    cout << "The size of the image " << file_name << " is " << l.getSize(file_name) << endl;
     pic.display();
   }catch( ImageNotFoundException &e){ cout << e.getMessage() << endl;}
 }
