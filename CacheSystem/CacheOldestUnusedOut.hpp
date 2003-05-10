6ba881e1595478e0e1c685d601e8c173cb2b0bb1
madmatt <madmatt>
2003-05-10 19:40:41 +0000

    Version mise à jour avec le cache FIFO. Ne compile probablement pas, et risque d'avoir des petites erreurs de fonctionnement. Mais normalement, il n'y a pas tellement à faire :).

--- CacheSystem/CacheOldestUnusedOut.hpp
+++ CacheSystem/CacheOldestUnusedOut.hpp
@@ -17,18 +17,18 @@ namespace CacheSystem{
    * which has the highest priority is removed.
    */
   template <class T, class K, class L>
-  class CacheOldestUnusedOut: public CacheSystem::Cache<T, K, L>{
+  class CacheOldestUnusedOut{
   private:
     ///////Attributes
-    int      _total_bytes; //Total number of bytes contained in the cache
-    int      _max_bytes;   //Maximum number of bytes
+    int       _total_bytes; //Total number of bytes contained in the cache
+    int       _max_bytes;   //Maximum number of bytes
     
-    int      _nb_of_images;     //Number of images contained in the cache
-    int      _max_nb_of_images; //Maximum number of images allowed
+    int       _nb_of_images;     //Number of images contained in the cache
+    int       _max_nb_of_images; //Maximum number of images allowed
     
-    L   _loader;      //The object that will enable this cache to load files in memory
+    L         _loader;      //The object that will enable this cache to load files in memory
     //Contains all the key/references to a Reference Counter
-    hash_map<const K, T, hash<const K> >  _image_set;
+    map<K, T> _image_set;
     list<pair<K*, int> > _freeable;    //Contains the key of all the freeable elements
     
     ///////Methods
@@ -36,10 +36,12 @@ namespace CacheSystem{
      * Adds an image to the list of images.
      * Returns true if the operation succeeded, false otherwise.
      */
-    bool addImageObject(const K &key, T/* A VERIFIER */ image) throw (NotEnoughSpaceException){
+    void addImageObject(const K &key, const T &image){
+      int image_size = this->_loader.getSize(key);
       this->_image_set[key] = image;
-      addToFreeable(key);
-      return true;
+      addToFreeable(&key);
+      this->_nb_of_images++;
+      this->_nb_of_bytes += image_size;
     }
     
     /**
@@ -49,18 +51,6 @@ namespace CacheSystem{
     bool removeImageObject(const K &key){
       if(this->_image_set.erase(key)>=1) return true;
       return false;
-    }
-    
-    /**
-     * Adds the image-object to the list of the freeable objects.
-     * Returns true if the operation succeeded, false otherwise.
-     */
-    bool addToFreeable(const K &key){
-      pair<K*, int> key_priority = pair<K*, int>(&key, 0); //Default priority = 0, and we need a pointer
-      
-      //The pairs that are in front of the list are to be freed in priority
-      this->_freeable.push_back(key_priority);
-      return true;
     }
     
     /**
@@ -75,21 +65,24 @@ namespace CacheSystem{
     /**
      * Frees some objects while there isn't enough space.
      */
-    void freeSomeMemory() throw (NotEnoughSpaceException)
+    void freeSomeMemory(int new_file_size) throw (NotEnoughSpaceException,
+						  RemovalImpossibleException)
     {
       pair<K*, int> image2bfreed;
       
       /* while there isn't enough space, we delete the first image-objects */
-      while(_nb_of_images>=_max_nb_of_images || _total_bytes>=_max_bytes)
+      while(_nb_of_images+1>=_max_images || _nb_of_bytes+new_file_size>=_max_bytes)
 	{//if there's too much image in cache
-	  if(this->_freeable.empty())//if the FIFO is empty, the user didn't grant enough space for the cache
+	  if(this->_freeable.empty())//if the list is empty, the user didn't grant enough space for the cache
 	    throw NotEnoughSpaceException();
 	  image2bfreed = _freeable.front();
+	  int image2bfreed_size = this->_loader.getSize(*image2bfreed);
 	  //remove an image from the cache
 	  if(!removeImageObject(*image2bfreed.first))
 	    throw RemovalImpossibleException(": removeImageObject.");
 	  if(!removeFromFreeable())
 	    throw RemovalImpossibleException(": removeFromFreeable.");
+	  this->_nb_of_bytes -= image2bfreed_size;
 	}
     }
     
@@ -97,7 +90,6 @@ namespace CacheSystem{
      * Decreases the priority of the image-object which has the key 'key'.
      * This method than shakes the freeable list, in order the freeable objects
      * at the beginning of the list.
-     * TESTABLE ISOLEMENT
      */
     void shakePriorities(const K &key) throw (Unable2ShakePriority)
     {
@@ -131,8 +123,56 @@ namespace CacheSystem{
     }
     
   public:
-    CacheOldestUnusedOut(const L &loader):_loader(loader){}
+    CacheOldestUnusedOut(const L &loader,
+			 int max_bytes, int max_nb_of_images):_nb_of_bytes(0),
+							      _max_bytes(max_bytes),
+							      _nb_of_images(0),
+							      _max_images(max_nb_of_images),
+							      _loader(loader){}
     ~CacheOldestUnusedOut(){}
+    
+    
+    /**
+     * Initialises the iterator on the list of objects contained in this cache.
+     */
+    void initIterator()
+    { this->_it = this->_image_set.begin(); }
+    
+    /**
+     * At each call, returns the key of an object contained in this cache.
+     */
+    K getEachKeyStored()
+    {
+      if(this->_it == this->_image_set.end())
+	return "";
+      K returned = this->_it->first;
+      this->_it++;
+      return returned;
+    }
+    
+    
+    /**
+     * Adds the image-object to the list of the freeable objects.
+     * Returns true if the operation succeeded, false otherwise.
+     */
+    bool addToFreeable(const K &key){
+      pair<K*, int> key_priority = pair<K*, int>(&key, 0); //Default priority = 0, and we need a pointer
+      
+      //The pairs that are in front of the list are to be freed in priority
+      this->_freeable.push_back(key_priority);
+      return true;
+    }
+    
+    
+    /**
+     * Sets the bounds of the cache(total number of bytes, maximum authorized number of
+     * bytes, number of images, maximum number of images).
+     */
+    void setBounds(int max_bytes, int max_nb_of_images)
+    {
+      this->_max_bytes  = max_bytes;
+      this->_max_images = max_nb_of_images;
+    }        
     
     
     /**
@@ -140,13 +180,19 @@ namespace CacheSystem{
      * Must be called by the user interface-side.
      */
     T getImageObject(const K &key)const{
-      hash_map<const K, T, hash<const K> >::iterator index = this->_image_set.find(key);
+      map<const K, T>::iterator index = this->_image_set.find(key);
+      
       if(index == _image_set.end())
-	{//the object-image isn't in the image set
-	  freeSomeMemory(); //Free EVENTUALLY some memory
+	{ //the object-image isn't in the image set
 	  //ASK THE LOADER to fetch the image
-	  /* VOIR AVEC RODRIGUE S'IL FAUT DONNER LA TAILLE DE L'OBJET A LA METHODE D'AJOUT */
-	  addImageObject(key, image); //ADD THE NEW image to the cache
+	  Pointer::StandardSmartPointer<CacheReferenceCounter<CacheOldestUnusedOut,K,T>, T> image ( new CacheReferenceCounter<CacheOldestUnusedOut,K,T>(this,key, this->_loader.getObject(key) ) );
+	  try{
+	    freeSomeMemory(this->_loader.getSize(key)); //eventually, free some memory
+	    addImageObject(key, *image); //ADD THE NEW image to the cache
+	    index = this->_image_set.find(key);
+	  }catch(NotEnoughSpaceException e){
+	    cerr<<e.getMessage()<<"\nImage "<<key<<" hasn't been added in the cache.";
+	    return image;
 	}
       else //If the element is already present, we only have to decrease his priority
 	shakePriorities(key); //and shake the freeable list
